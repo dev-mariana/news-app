7e719b88acf55295736f3de837226aedb655ecdd
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-04 16:21:46 -0300

    chore: add project and configs

---
+++ backend/jest.config.ts
@@ -0,0 +1,16 @@
+import { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+}
+
+export default config
