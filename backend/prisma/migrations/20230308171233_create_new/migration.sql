7a7e14f94675c84475e821de6294d6fcd8b93d5e
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-08 14:14:10 -0300

    refactor: change properties in schema from prisma

--- backend/prisma/migrations/20230308171233_create_new/migration.sql
+++ backend/prisma/migrations/20230308171233_create_new/migration.sql
@@ -4,11 +4,14 @@ CREATE TABLE "New" (
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "type" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "writer" TEXT NOT NULL,
 
     CONSTRAINT "New_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "New_id_key" ON "New"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "New_title_key" ON "New"("title");
