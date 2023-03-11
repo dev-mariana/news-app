b2201e49d5a32a49c9054504e6ea0a8b644b9805
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-10 21:39:58 -0300

    test: add create new on service

---
+++ backend/src/application/new/new.service.spec.ts
@@ -0,0 +1,21 @@
+import { New, PrismaClient } from '@prisma/client'
+import { NewService } from './services/new.service'
+import { v4 as uuid } from 'uuid'
+
+describe('NewService tests', () => {
+  const prisma = new PrismaClient()
+  const newService = new NewService(prisma)
+
+  it('Should create a new', async () => {
+    const data: New = {
+      id: uuid(),
+      title: 'Test',
+      description: 'test',
+      type: 'teste',
+      created_at: new Date(),
+      writer: 'teste',
+    }
+    const result = await newService.create(data)
+    expect(result).toEqual(data)
+  })
+})
