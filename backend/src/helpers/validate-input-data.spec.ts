14157d11443728cf13bab5435c0788099ff67f19
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-14 23:26:55 -0300

    test: add Should return message iguals writer is missing if writer is missing

--- backend/src/helpers/validate-input-data.spec.ts
+++ backend/src/helpers/validate-input-data.spec.ts
@@ -3,7 +3,6 @@ import { validateData } from './validate-input-data'
 describe('ValidateInputData Helper test', () => {
   it('Should return message iguals title is missing if title is missing', () => {
     const data = {
-      id: '3d496809-a5db-4885-a87e-d4c477890f58',
       description: 'test',
       type: 'teste',
       writer: 'teste',
@@ -16,7 +15,6 @@ describe('ValidateInputData Helper test', () => {
 
   it('Should return message iguals description is missing if description is missing', () => {
     const data = {
-      id: '3d496809-a5db-4885-a87e-d4c477890f58',
       title: 'Test',
       type: 'teste',
       writer: 'teste',
@@ -29,12 +27,23 @@ describe('ValidateInputData Helper test', () => {
 
   it('Should return message iguals type is missing if type is missing', () => {
     const data = {
-      id: '3d496809-a5db-4885-a87e-d4c477890f58',
       title: 'Test',
       description: 'test',
       writer: 'teste',
     }
     const message = 'Is missing type field.'
+    const result = validateData(data)
+
+    expect(result).toEqual(message)
+  })
+
+  it('Should return message iguals writer is missing if writer is missing', () => {
+    const data = {
+      title: 'Test',
+      type: 'teste',
+      description: 'test',
+    }
+    const message = 'Is missing writer field.'
     const result = validateData(data)
 
     expect(result).toEqual(message)
