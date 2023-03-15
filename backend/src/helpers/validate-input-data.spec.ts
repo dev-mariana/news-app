dd539f159d6db43ebdec4a3ffe1efa4e7a179c7a
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-14 23:23:09 -0300

    test: add Should return message iguals type is missing if type is missing

--- backend/src/helpers/validate-input-data.spec.ts
+++ backend/src/helpers/validate-input-data.spec.ts
@@ -26,4 +26,17 @@ describe('ValidateInputData Helper test', () => {
 
     expect(result).toEqual(message)
   })
+
+  it('Should return message iguals type is missing if type is missing', () => {
+    const data = {
+      id: '3d496809-a5db-4885-a87e-d4c477890f58',
+      title: 'Test',
+      description: 'test',
+      writer: 'teste',
+    }
+    const message = 'Is missing type field.'
+    const result = validateData(data)
+
+    expect(result).toEqual(message)
+  })
 })
