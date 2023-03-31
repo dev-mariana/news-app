368d6b4c2218b6ac1c56cbcc510fbffa09d61ddc
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-30 22:22:54 -0300

    refactor: remove duplicate code

--- backend/src/application/new/controllers/new.controller.spec.ts
+++ backend/src/application/new/controllers/new.controller.spec.ts
@@ -1,84 +1,43 @@
 import { NewController } from './new.controller'
 import { NewService } from '../services/new.service'
-import { PrismaClient } from '@prisma/client'
+import { New, PrismaClient } from '@prisma/client'
 import { Request, Response } from 'express'
+import {
+  newData,
+  requestMock,
+  requestMock2,
+  responseMock,
+  serviceMock,
+} from '../mocks/mock'
 
 describe('NewController tests', () => {
   const prisma = new PrismaClient()
   const newService = new NewService(prisma)
   const newController = new NewController()
+  const data = newData
 
-  beforeEach(async () => {
-    const mockRequest = {
-      body: {
-        id: '123',
-        title: 'Test New',
-        description: 'This is a test new',
-        type: 'test',
-        created_at: new Date(),
-        writer: 'Test Writer',
-      },
-    } as Request
-
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
-    } as unknown as Response
-
-    await newController.createNew(mockRequest, mockResponse)
+  beforeAll(async () => {
+    await prisma.$connect()
+    await newService.create(data)
   })
 
-  beforeAll(async () => await prisma.$connect())
-  afterAll(async () => await prisma.$disconnect())
-
-  afterEach(async () => {
+  afterAll(async () => {
     await prisma.new.deleteMany()
-    jest.resetAllMocks()
+    await prisma.$disconnect()
   })
 
   it('Should create a new if not have errors', async () => {
-    const mockRequest = {
-      body: {
-        id: '123',
-        title: 'Test New 2',
-        description: 'This is a test new',
-        type: 'test',
-        created_at: new Date(),
-        writer: 'Test Writer',
-      },
-    } as Request
-
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Response
+    const mockRequest: Request = requestMock
+    const mockResponse: Response = responseMock
 
     await newController.createNew(mockRequest, mockResponse)
-
     expect(mockResponse.status).toHaveBeenCalledWith(201)
   })
 
   it('Should return 400 if any required field is missing', async () => {
-    const mockRequest = {
-      body: {
-        id: '123',
-        description: 'This is a test new',
-        type: 'test',
-        created_at: new Date(),
-        writer: 'Test Writer',
-      },
-    } as Request
-
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-      send: jest.fn(),
-    } as unknown as Response
-
-    const mockService = {
-      findOne: jest.fn().mockReturnValue(null),
-      create: jest.fn(),
-    }
+    const mockRequest: Request = requestMock2
+    const mockResponse: Response = responseMock
+    const mockService = serviceMock
 
     await newController.createNew(mockRequest, mockResponse)
 
@@ -91,49 +50,22 @@ describe('NewController tests', () => {
   })
 
   it('Should throw an error if title already exists', async () => {
-    const mockRequest = {
-      body: {
-        id: '123',
-        title: 'Test New',
-        description: 'This is a test new',
-        type: 'test',
-        created_at: new Date(),
-        writer: 'Test Writer',
-      },
-    } as Request
-
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
-    } as unknown as Response
+    const mockRequest: Request = requestMock
+    const mockResponse: Response = responseMock
 
     await newController.createNew(mockRequest, mockResponse)
 
     expect(mockResponse.status).toHaveBeenCalledWith(500)
     expect(mockResponse.send).toHaveBeenCalledWith(
-      'Error: The new with name Test New already exists.'
+      'Error: The new with name Test New 2 already exists.'
     )
   })
 
   it('Should return a list of news if not have errors', async () => {
-    const mockRequest = {
-      body: {
-        id: '123',
-        title: 'Test New',
-        description: 'This is a test new',
-        type: 'test',
-        created_at: new Date(),
-        writer: 'Test Writer',
-      },
-    } as Request
-
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
-    } as unknown as Response
+    const mockRequest: Request = requestMock
+    const mockResponse: Response = responseMock
 
     await newController.findAll(mockRequest, mockResponse)
-
     expect(mockResponse.status).toHaveBeenCalledWith(201)
   })
 })
--- backend/src/application/new/mocks/mock.ts
+++ backend/src/application/new/mocks/mock.ts
@@ -1,4 +1,5 @@
 import { New } from '@prisma/client'
+import { Request, Response } from 'express'
 
 export const dataMock = {
   id: '123',
@@ -17,3 +18,44 @@ export const resultMock = {
   created_at: null,
   writer: 'teste',
 } as unknown as New
+
+export const requestMock = {
+  body: {
+    id: '123',
+    title: 'Test New 2',
+    description: 'This is a test new',
+    type: 'test',
+    created_at: new Date(),
+    writer: 'Test Writer',
+  },
+} as unknown as Request
+
+export const responseMock = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+  send: jest.fn(),
+} as unknown as Response
+
+export const requestMock2 = {
+  body: {
+    id: '123',
+    description: 'This is a test new',
+    type: 'test',
+    created_at: new Date(),
+    writer: 'Test Writer',
+  },
+} as unknown as Request
+
+export const serviceMock = {
+  findOne: jest.fn().mockReturnValue(null),
+  create: jest.fn(),
+}
+
+export const newData: New = {
+  id: '123',
+  title: 'Test New',
+  description: 'This is a test new',
+  type: 'test',
+  created_at: new Date(),
+  writer: 'Test Writer',
+}
