f23b02e5a4ac9a38c5a7cf0cc9333738f3ea26b6
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-07-15 22:26:08 -0300

    refactor: change property name at journalist model

--- backend - v2/prisma/migrations/20230716012530_create_journalist/migration.sql
+++ backend - v2/prisma/migrations/20230716012530_create_journalist/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "Journalist" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "lastname" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
