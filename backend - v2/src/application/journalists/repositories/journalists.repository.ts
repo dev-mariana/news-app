2b64ea21e11dbdd4525db656490926e80471f944
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-07-16 00:20:01 -0300

    feat: add journalists repository

---
+++ backend - v2/src/application/journalists/repositories/journalists.repository.ts
@@ -0,0 +1,25 @@
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+import { CreateJournalistDTO } from '../dto/create-journalist.dto';
+import { v4 as uuid } from 'uuid';
+import { JournalistEntity } from '../entities/journalist.entity';
+import { Prisma } from '@prisma/client';
+import { encryptPassword } from '@helpers/encrypt-password';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JournalistsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(journalistDTO: CreateJournalistDTO): Promise<JournalistEntity> {
+    const encryptedPassword = await encryptPassword(journalistDTO.password);
+    const data: Prisma.JournalistCreateInput = {
+      id: uuid(),
+      name: journalistDTO.name,
+      last_name: journalistDTO.last_name,
+      email: journalistDTO.email,
+      password: encryptedPassword,
+    };
+
+    return await this.prisma.journalist.create({ data });
+  }
+}
